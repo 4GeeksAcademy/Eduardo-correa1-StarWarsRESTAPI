4eb3b227f8cd0edebc86af53422f9a1d3e7299a8
Eduardo-correa1 <159133054+Eduardo-correa1@users.noreply.github.com>
2024-07-26 19:01:44 +0000

    eliminada funcion de ejemplo

--- src/app.py
+++ src/app.py
@@ -36,14 +36,6 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
 
 # get characters
 @app.route("/characters",  methods=['GET'])
